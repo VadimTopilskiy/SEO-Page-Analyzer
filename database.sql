9a8624b060c0e7eca464edc28ed5d2b8af3790bc
Vadim <topilskiy.v1@gmail.com>
2023-12-05 15:09:50 +0300

    added url_checks table

--- database.sql
+++ database.sql
@@ -3,3 +3,16 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255) UNIQUE NOT NULL,
+  );
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id REFERENCES urls (id),
+    status_code BIGINT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP NOT NULL
+);

20f0323fd9a04f590c8c90e42134a7c85a3842aa
Vadim <topilskiy.v1@gmail.com>
2023-12-17 14:20:26 +0300

    update project

--- database.sql
+++ database.sql
@@ -1,19 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255) UNIQUE NOT NULL,
-  );
-
-DROP TABLE IF EXISTS url_checks;
+  created_at TIMESTAMP NOT NULL
+);
 
 CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id REFERENCES urls (id),
     url_id BIGINT REFERENCES urls (id),
     status_code BIGINT,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
-    description VARCHAR(255),
+    h1 text,
+    title text,
+    description text,
     created_at TIMESTAMP NOT NULL
 );
